8cc5f4a0659a719c9ec0f0ed127da83d4d57b5b0
Paulo Bruny Lima <paulobruny@gmail.com>
2021-03-08 18:24:27 -0300

    add info icon to show about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,29 @@
+import LinkWrapper from 'components/LinkWrapper'
 import dynamic from 'next/dynamic'
+
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return (
-    <Map
-      places={[
-        {
-          id: '1',
-          name: 'Petrolina',
-          slug: 'petrolina',
-          location: {
-            latitude: -9.38,
-            longitude: -40.5
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map
+        places={[
+          {
+            id: '1',
+            name: 'Petrolina',
+            slug: 'petrolina',
+            location: {
+              latitude: -9.38,
+              longitude: -40.5
+            }
           }
-        }
-      ]}
-    />
+        ]}
+      />
+    </>
   )
 }
