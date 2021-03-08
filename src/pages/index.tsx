303400e128069462f4c2fcf8b15d73e27ccf3e50
Paulo Bruny Lima <paulobruny@gmail.com>
2021-03-08 18:01:07 -0300

    create and add map with test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,19 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  return (
+    <Map
+      places={[
+        {
+          id: '1',
+          name: 'Petrolina',
+          slug: 'petrolina',
+          location: {
+            latitude: -9.38,
+            longitude: -40.5
+          }
+        }
+      ]}
+    />
+  )
 }
