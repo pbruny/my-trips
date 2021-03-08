223b6ddf39a72586548c8f884091f81ed5873431
Paulo Bruny Lima <paulobruny@gmail.com>
2021-03-08 20:39:28 -0300

    configure basic structure graphql request and next

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,19 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
 const About = () => {
   return <AboutTemplate />
+}
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
 }
 
 export default About
